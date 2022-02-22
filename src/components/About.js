25c3bb1c9e20b9cbfe43a8795627abbd2055b4c1
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-21 22:42:50 -0500

    new app

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='ui raised very padded text container segment'
+            style={{ marginTop: '80px' }}>
+            <h3 className='ui header'>About</h3>
+            <p>Lorem ipsum dolor sit amet</p>
+        </div>
+    )
+}
+
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='ui raised very padded text container segment'
+            style={{ marginTop: '80px' }}>
+            <h3 className='ui header'>Contact</h3>
+            <p>Lorem ipsum dolor sit amet</p>
+        </div>
+    )
+}
+
+
+export default Contact;
