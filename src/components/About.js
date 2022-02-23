ee3d27c9d1cc457456c04ff20821015a67cbe766
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-23 11:28:39 -0500

    add Modal component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+// import './Modal.css';
 const About = () => {
     return (
-        <div className='ui raised very padded text container segment'
-            style={{ marginTop: '80px' }}>
-            <h3 className='ui header'>About</h3>
-            <p>Lorem ipsum dolor sit amet</p>
+        <div>
+            {/* <div className='modal'>
+                <div className='modal-content'>
+                    <h3> I am modal </h3>
+                </div>
+            </div> */}
+            <div className='ui raised very padded text container segment'
+                style={{ marginTop: '80px' }}>
+                <h3 className='ui header'>About</h3>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+
         </div>
+
     )
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Modal = (props) => {
+
+    return ReactDOM.createPortal(
+        <div className='ui dimmer show modals visible active'>
+            <div className='ui raised very padded text container segment'>
+                <h3> I am Modal</h3>
+            </div>
+        </div>
+        , document.querySelector('#modal')
+    )
+}
+
+export default Modal;
+
+
